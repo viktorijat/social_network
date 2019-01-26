8ccb7eabd78f43f316467f07d9253456d1349f79
viktorija <tosheva.victoria@yahoo.com>
2019-01-26 15:15:59 +0100

    first commit

---
+++ src/main/java/com/learning/social/learningsocial/LearningsocialApplication.java
@@ -0,0 +1,14 @@
+package com.learning.social.learningsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningsocialApplication.class, args);
+	}
+
+}
+
